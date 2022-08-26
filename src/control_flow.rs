1497349480bcff8d95ed5d663431e1feadab1172
Daniel Liu <mr.picklepinosaur@gmail.com>
2022-08-26 18:02:29 -0400

    control flow functions with error function

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -11,6 +11,20 @@ macro_rules! ok_or_return {
     };
 }
 
+/// Unwraps result type or returns with error function
+#[macro_export]
+macro_rules! ok_or_return_msg {
+    ( $e:expr, $f:expr ) => {
+        match $e {
+            Ok(x) => x,
+            Err(e) => {
+                $f(e);
+                return
+            },
+        }
+    };
+}
+
 /// Unwraps result type or continues
 #[macro_export]
 macro_rules! ok_or_continue {
@@ -18,6 +32,20 @@ macro_rules! ok_or_continue {
         match $e {
             Ok(x) => x,
             Err(_) => continue,
+        }
+    };
+}
+
+/// Unwraps result type or continues with error function
+#[macro_export]
+macro_rules! ok_or_continue_msg {
+    ( $e:expr, $f:expr ) => {
+        match $e {
+            Ok(x) => x,
+            Err(e) => {
+                $f(e);
+                continue
+            },
         }
     };
 }
@@ -33,6 +61,20 @@ macro_rules! some_or_return {
     };
 }
 
+/// Unwraps option type or returns with error function
+#[macro_export]
+macro_rules! some_or_return_msg {
+    ( $e:expr, $f:expr ) => {
+        match $e {
+            Some(x) => x,
+            None => {
+                $f();
+                return
+            },
+        }
+    };
+}
+
 /// Unwraps option type or continues
 #[macro_export]
 macro_rules! some_or_continue {
@@ -40,6 +82,20 @@ macro_rules! some_or_continue {
         match $e {
             Some(x) => x,
             None => continue,
+        }
+    };
+}
+
+/// Unwraps option type or continues with error function
+#[macro_export]
+macro_rules! some_or_continue_msg {
+    ( $e:expr, $f:expr ) => {
+        match $e {
+            Some(x) => x,
+            None => {
+                $f();
+                continue
+            },
         }
     };
 }
