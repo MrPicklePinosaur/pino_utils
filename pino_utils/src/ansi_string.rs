13063bb24d7722fd497651e10d64ce9c63694036
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-02-16 19:27:39 -0500

    more ergonomic ansi_string

--- pino_utils/src/ansi_string.rs
+++ pino_utils/src/ansi_string.rs
@@ -1,6 +1,9 @@
 //! Utilities for constructing ANSI strings
 
+use std::{default, fmt::Display};
+
 /// The 16 ANSI colors
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Color {
     Black = 30,
     Red = 31,
@@ -21,7 +24,14 @@ pub enum Color {
     StrongWhite = 97,
 }
 
+impl Default for Color {
+    fn default() -> Self {
+        Color::White
+    }
+}
+
 /// ANSI effects
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Effect {
     Normal,
     Bold,
@@ -44,9 +54,94 @@ impl Effect {
     }
 }
 
+impl Default for Effect {
+    fn default() -> Self {
+        Effect::Normal
+    }
+}
+
 /// Construct a new ANSI string from a color and an effect
+/// Deprecated
 pub fn ansi_string(s: &str, color: Color, effect: Effect) -> String {
     let color_val = color as isize;
     let effect_val = effect.value();
     format!("\x1b[{}{}m{}\x1b[0m", effect_val, color_val, s)
 }
+
+#[derive(Default)]
+pub struct AnsiString {
+    pub value: String,
+    pub color: Color,
+    pub effect: Effect,
+}
+
+impl AnsiString {}
+
+impl Display for AnsiString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.color == Color::White && self.effect == Effect::Normal {
+            write!(f, "{}", self.value)
+        } else {
+            let color_val = self.color as isize;
+            let effect_val = self.effect.value();
+            write!(f, "\x1b[{}{}m{}\x1b[0m", effect_val, color_val, self.value)
+        }
+    }
+}
+
+trait ToAnsiString {
+    fn normal(self) -> AnsiString;
+    fn bold(self) -> AnsiString;
+    fn dim(self) -> AnsiString;
+    fn underline(self) -> AnsiString;
+    fn blinking(self) -> AnsiString;
+    fn reversed(self) -> AnsiString;
+}
+
+impl ToAnsiString for String {
+    fn normal(self) -> AnsiString {
+        AnsiString {
+            value: self,
+            color: Color::White,
+            effect: Effect::Normal,
+        }
+    }
+    fn bold(self) -> AnsiString {
+        AnsiString {
+            value: self,
+            color: Color::White,
+            effect: Effect::Bold,
+        }
+    }
+    fn dim(self) -> AnsiString {
+        AnsiString {
+            value: self,
+            color: Color::White,
+            effect: Effect::Dim,
+        }
+    }
+    fn underline(self) -> AnsiString {
+        AnsiString {
+            value: self,
+            color: Color::White,
+            effect: Effect::Underline,
+        }
+    }
+    fn blinking(self) -> AnsiString {
+        AnsiString {
+            value: self,
+            color: Color::White,
+            effect: Effect::Blinking,
+        }
+    }
+    fn reversed(self) -> AnsiString {
+        AnsiString {
+            value: self,
+            color: Color::White,
+            effect: Effect::Reversed,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {}
